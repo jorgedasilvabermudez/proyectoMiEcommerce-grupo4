51a4b2ac2806cc38ab8adea5523c6968c54ab97d
jorgedasilvabermudez <jorge.dasilvabermudez@cencosud.com>
2022-09-12 12:40:57 -0300

    ruta y app

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app= express();
+const router= express.Router();
+const products= require('./products.json')
+console.log(products);
+
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views','./views/pages');
+
+//rutas
+app.get("/", (req, res) => { res.render('index');  });
+app.get("/products", (req, res) => { res.render('product');  });
+app.get("/cart", (req, res) => { res.render('cart');  });
+app.get("/checkout", (req, res) => { res.render('checkout');  });
+app.get("/register", (req, res) => { res.render('register');  });
+app.get("/login", (req, res) => { res.render('login');  });
+
+
+
+
+app.listen(3000,()=>{
+    console.log('Ecommerce started')
+})
