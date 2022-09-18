c3e014106da8d0cf172f95ec3b8dd2e6776e61df
jorgedasilvabermudez <jorge.dasilvabermudez@cencosud.com>
2022-09-17 22:43:17 -0300

    logica y rutas de product-products

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
 const express = require("express");
 const app = express();
-const routes = require("./src/routes");
-const products = require("./products.json");
+const router = require("./src/routes");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./src/views/pages");
 
 //rutas
-
-app.get("/", (req, res) => {
-  res.render("index", { products: products });
-});
-app.get("/products", (req, res) => {
-  res.render("product", { products: products });
-});
-app.get("/cart", (req, res) => {
-  res.render("cart", { products: products });
-});
-app.get("/checkout", (req, res) => {
-  res.render("checkout", { products: products });
-});
-app.get("/register", (req, res) => {
-  res.render("register", { products: products });
-});
-app.get("/login", (req, res) => {
-  res.render("login", { products: products });
-});
-
-/*routes(app);*/
+app.use('/',router)
 
 app.listen(3000, () => {
   console.log("Ecommerce started");
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,13 +1,20 @@
 const fetch = require("node-fetch");
 
 const getProduct = async (req, res) => {
-  //Logica para obtener producto
-  res.render("index", { products });
+  //articulo por id
+  const { id }= req.params;
+  const result= await fetch(`http://localhost:6000/api/product/${id}`);
+  const data= await result.json();
+  //articulos para productCard
+  const results= await fetch(`http://localhost:6000/api/product`);
+  const datas = await results.json();
+  res.render("product", { product : data, products: datas });
 };
 
 const getProducts = async (req, res) => {
-  //Logica para obtener productos
-  res.render("products", { product });
+  const result= await fetch(`http://localhost:6000/api/product`);
+  const data = await result.json();
+  res.render("index", { products : data });
 };
 
 module.exports = {
