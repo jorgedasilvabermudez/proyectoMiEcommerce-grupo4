b79a464abb704946f8d3033f6c8c7f3aa91a031a
jorgedasilvabermudez <jorge.dasilvabermudez@cencosud.com>
2022-09-18 16:41:27 -0300

    history 7 y 8

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -8,13 +8,18 @@ const getProduct = async (req, res) => {
   //articulos para productCard
   const results= await fetch(`http://localhost:6000/api/product`);
   const datas = await results.json();
+
   res.render("product", { product : data, products: datas });
 };
 
 const getProducts = async (req, res) => {
   const result= await fetch(`http://localhost:6000/api/product`);
   const data = await result.json();
-  res.render("index", { products : data });
+  //mas pedidos
+  const ordered= data.sort( (a, b)=>b.rating.count-a.rating.count);
+  const moreCount=ordered.slice(0,8)
+  
+  res.render("index", { products : data, moreCount });
 };
 
 module.exports = {
