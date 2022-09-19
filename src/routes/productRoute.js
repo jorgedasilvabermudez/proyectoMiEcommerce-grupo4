5f4bb6f74bbb6314d52a79b0af863759483c40ca
soriagorgoroso <112647797+soriacencosud@users.noreply.github.com>
2022-09-19 15:35:13 -0300

    Controllador prodcuts y Services

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -2,10 +2,9 @@ const express = require("express");
 const productRouter = express.Router();
 const productController = require("../controllers/productController");
 
-productRouter.get("/", productController.getProducts);
-productRouter.get("/products/:id", productController.getProduct);
+productRouter.get("/", productController.index);
+productRouter.get("/products/:id", productController.product);
 
 module.exports = productRouter;
-
 
 //archivo en singular
