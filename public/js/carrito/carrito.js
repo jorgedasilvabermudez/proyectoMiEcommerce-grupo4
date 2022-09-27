cd10fdcf51351e97b486828a627e7a3cfef48c3e
maxiCaballo <pablo.caballo@cencosud.com>
2022-09-27 17:42:40 -0300

    Sprint 6 terminado, refactorizar y ordenar

--- public/js/carrito/carrito.js
+++ public/js/carrito/carrito.js
@@ -1,7 +1,3 @@
-const eliminarProducto = document.querySelector(".buttonQuitar");
-const restarProducto = document.querySelector("#restarProducto");
-const sumarProducto = document.querySelector("#sumarProducto");
-
 // -------- Datos de prueba ---------
 const usuario = JSON.stringify({
   id: 0,
@@ -12,34 +8,44 @@ const usuario = JSON.stringify({
 //localStorage.removeItem("usuario");
 const usuarioEnLocalStorage = JSON.parse(localStorage.getItem("usuario"));
 // -------- Datos de prueba ---------
+
 window.addEventListener("load", async () => {
   try {
     const userCart = await getUserCart(usuarioEnLocalStorage.id); //Carrito como viene de la API
     //Si hay error redirect a Index
     if (userCart.status === 404)
       window.location.href = `${window.location.origin}`;
+
     const userCartDetail = await getUserCartDetail(userCart); //Carrito con detalle para mostrar en vista
-    pintarProductosEnVista(userCartDetail);
-    console.log(userCartDetail);
+    pintarProductosEnVista(userCartDetail, userCart); //User cart es para pintar la cantidad
+
+    const restarProducto = document.querySelectorAll(".restarProducto");
+    const sumarProducto = document.querySelectorAll(".sumarProducto");
+    const eliminarProducto = document.querySelectorAll(".buttonQuitar");
+
+    eliminarProducto.forEach((btn) => {
+      btn.addEventListener("click", async () => {
+        await deleteProduct(btn.dataset.id);
+        document.location.reload();
+      });
+    });
+    sumarProducto.forEach((btn) => {
+      btn.addEventListener("click", async () => {
+        await addProduct(btn.dataset.id, userCart);
+        document.location.reload();
+      });
+    });
+    restarProducto.forEach((btn) => {
+      btn.addEventListener("click", async () => {
+        await removeOneProduct(btn.dataset.id, userCart);
+        document.location.reload();
+      });
+    });
   } catch (error) {
     console.log(error);
     return error;
   }
 });
-//Eliminar producto
-eliminarProducto.addEventListener("click", async () => {
-  try {
-    console.log(e);
-    //await deleteProduct();
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
-});
-//Sumar producto
-sumarProducto.addEventListener("click", () => {});
-//Restar producto
-restarProducto.addEventListener("click", () => {});
 
 //FUNCIONES
 //Api carrito sin detalle
@@ -77,32 +83,18 @@ async function getProductById(productId) {
     return error;
   }
 }
-async function deleteProduct(idProducto) {
-  console.log(idProducto);
-  try {
-    const response = await fetch(
-      `http://localhost:4000/api/cart/${usuarioEnLocalStorage.id}?productId=${idProducto}`,
-      {
-        method: "DELETE",
-      }
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
-}
-function pintarProductosEnVista(cartDetail) {
+function pintarProductosEnVista(cartDetail, userCart) {
   const section = document.querySelector("#cartSection");
   let fragment = document.createDocumentFragment();
   let totalPrice = 0;
   //Articles
   for (const product of cartDetail) {
+    let { quantity } = userCart.find((element) => element.id === product.id);
+
     let article = document.createElement("article");
     totalPrice += product.price;
     article.innerHTML = `
-    <article class="articleCard" data-id="${product.id}">
+    <article class="articleCard">
     <div class="cardInfromation">
       <img
         class="card__img"
@@ -112,10 +104,10 @@ function pintarProductosEnVista(cartDetail) {
       <p>${product.description}</p>
     </div>
     <div class="cardButton">
-      <button class="buttonQuitar">Quitar</button>
+      <button class="buttonQuitar" data-id="${product.id}">Quitar</button>
       <div class="counter">
-        <button id="restarProducto">-</button><span>2</span
-        ><button id="sumarProducto">+</button>
+        <button class="restarProducto" data-id="${product.id}">-</button><span>${quantity}</span
+        ><button class="sumarProducto" data-id="${product.id}">+</button>
       </div>
       <p>${product.price}</p>
     </div>
@@ -141,4 +133,72 @@ function pintarProductosEnVista(cartDetail) {
   fragment.appendChild(divPagar);
 
   section.appendChild(fragment);
+}
+async function deleteProduct(idProducto) {
+  try {
+    const response = await fetch(
+      `http://localhost:4000/api/cart/${usuarioEnLocalStorage.id}?productId=${idProducto}`,
+      {
+        method: "DELETE",
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+async function addProduct(idProducto, userCart) {
+  try {
+    let { quantity } = userCart.find(({ id }) => id === Number(idProducto));
+
+    const body = {
+      userId: usuarioEnLocalStorage.id,
+      product: {
+        id: Number(idProducto),
+        quantity: quantity + 1,
+      },
+    };
+    const response = await fetch(`http://localhost:4000/api/cart`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+async function removeOneProduct(idProducto, userCart) {
+  try {
+    let { quantity } = userCart.find(({ id }) => id === Number(idProducto));
+    if (quantity === 1) {
+      return await deleteProduct(idProducto);
+    } else {
+      const body = {
+        userId: usuarioEnLocalStorage.id,
+        product: {
+          id: Number(idProducto),
+          quantity: quantity - 1,
+        },
+      };
+      const response = await fetch(`http://localhost:4000/api/cart`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      const data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
